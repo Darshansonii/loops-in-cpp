c96e3c4a45958d04877160ff49880277c952ef17
Darshansonii <darsan779d.dd@gmail.com>
2025-07-27 04:53:53 -0400

    Add files via upload

---
+++ Print SIT five times.cpp
@@ -0,0 +1,24 @@
+//Name-Darshan Soni
+//PRN-24070123034
+//Exp5
+
+//Printing SIT 5 Times
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<6; i++){
+        cout<<"SIT"<<"\n";
+    }
+    return 0;
+}
+
+/*
+Output
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
