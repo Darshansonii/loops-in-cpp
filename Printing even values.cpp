1941a890b970d383cf7153d6eba998ab3a0fa134
Darshansonii <darsan779d.dd@gmail.com>
2025-07-27 04:54:13 -0400

    Add files via upload

---
+++ Printing even values.cpp
@@ -0,0 +1,23 @@
+//Name-Darshan Soni
+//PRN-24070123034
+//Exp5
+
+//Printing even values
+
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i=2; i<=10; i+=2) {
+        cout << i<<endl;
+    }
+    return 0;
+}
+
+/*
+Output
+2
+4
+6
+8
+10
+*/
